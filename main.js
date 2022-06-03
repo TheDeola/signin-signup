d9e64292711843d58980bec6c7082e3d1a9c2edd
TheDeola <aadetunji012@gmail.com>
2022-06-03 17:53:44 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// This is my Javascript file
+
+let signinBtn = document.querySelector(".signinBtn")
+let signupBtn = document.querySelector(".signupBtn")
+
+let signinform = document.querySelector(".container1")
+let signupform = document.querySelector(".container2")
+
+console.log(signupBtn);
+
+signupBtn.addEventListener("click", function(){
+    signinform.classList.add("hide");
+    signupform.classList.remove("hide");
+})
+
+signinBtn.addEventListener("click", function(){
+    signupform.classList.add("hide");
+    signinform.classList.remove("hide");
+})
